79fc0e0b6eb292a8cf98ece80b5b6c2bc10a167b
Anmo9776 <112003314+Anmo9776@users.noreply.github.com>
2025-04-05 21:20:39 +0530

    Update aura.lua

--- modules/aura.lua
+++ modules/aura.lua
@@ -1,1 +1,40 @@
+-- modules/aura.lua
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local function enableAura()
+    if not getgenv().DinkzSettings.AutoAura then return end
+
+    local character = LocalPlayer.Character
+    if not character then return end
+
+    local hasAura = false
+
+    for _, v in pairs(character:GetChildren()) do
+        if v:IsA("Accessory") and v.Name:find("Armament") then
+            hasAura = true
+            break
+        end
+    end
+
+    if not hasAura then
+        local event = ReplicatedStorage:FindFirstChild("Remotes") and ReplicatedStorage.Remotes:FindFirstChild("Buso")
+        if event then
+            event:FireServer()
+        end
+    end
+end
+
+-- Run on character added
+LocalPlayer.CharacterAdded:Connect(function()
+    task.wait(1)
+    enableAura()
+end)
+
+-- Periodically check aura every 5 seconds
+while true do
+    task.wait(5)
+    enableAura()
+end
